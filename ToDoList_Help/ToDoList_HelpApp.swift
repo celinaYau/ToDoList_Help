dcfa56abf8f923faa4aee49377558a59609f8b7d
Celina <celinayau28@gmail.com>
2023-06-19 14:38:13 -0500

    Initial Commit

---
+++ ToDoList_Help/ToDoList_HelpApp.swift
@@ -0,0 +1,23 @@
+//
+//  ToDoList_HelpApp.swift
+//  ToDoList_Help
+//
+//  Created by scholar on 6/19/23.
+//
+
+//** This file is where will we access Core Data in Iteration 2**
+
+import SwiftUI
+
+@main
+struct ToDoList_HelpApp: App {
+    
+    //Add let persistenceController statement here
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            //Attach the environment modifier inside the parenthesis of ContentView() above
+        }
+    }
+}
